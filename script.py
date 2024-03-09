6e5c5ebb4d044399d5b921e6cfba2ad757b8ecb0
homuwell <homuwell@raspberrypi.com>
2024-03-09 14:23:25 +0300

    Added second lidar

--- script.py
+++ script.py
@@ -9,7 +9,8 @@ ultrasonic_pins = {
     'ultrasonic_sensor2': {'trigger': 31, 'echo': 33}
 }
 lidars_config = {
-    'lidar': {'port': '/dev/ttyAMA0', 'braudrate': '115200'}
+    'lidar': {'port': '/dev/ttyUSB0', 'braudrate': '115200'},
+    'lidar1': {'port': '/dev/ttyAMA0', 'braudrate': '115200'}
 }
 
 def print_sectors():
@@ -76,6 +77,7 @@ def clear_lines_from_bottom(num_lines):
         sys.stdout.write("\033[K")  # Clear line
 
 def getSectors(numbers):
+    print(numbers)
     left_inf, right_inf, left_lid, right_lid = numbers
     result = ""
 
@@ -107,12 +109,15 @@ for sensor, pins in ultrasonic_pins.items():
     GPIO.setup(pins['echo'], GPIO.IN)
 
 lidars_serials = {}
+
 for name, options in lidars_config.items():
     ser = serial.Serial()
     ser.port = options['port']
     ser.baudrate = options['braudrate']
     ser.open()
     lidars_serials[name] = ser
+    
+
 
 def getUltrasonicDistance(trigger_pin, echo_pin):
     GPIO.output(trigger_pin, False)
@@ -149,6 +154,7 @@ def getLidarDistance(ser):
         recv = ser.read(9)   
         ser.reset_input_buffer() 
         if recv[0] == 0x59 and recv[1] == 0x59:
+            print(recv)
             distance = recv[2] + recv[3] * 256
             ser.reset_input_buffer()
             return distance
@@ -176,7 +182,7 @@ try:
             else:
                 print(f"Ultrasonic {sensor} - Distanse: {dist} cm")
                 distances.append(dist)
-
+                
         for name, ser in lidars_serials.items():
             dist = getLidarDistance(ser)
             if dist == -1:
@@ -188,16 +194,16 @@ try:
             else:
                 print(f"lidar {name} - Distance: {dist} cm")
                 distances.append(dist)
-        distances.append(0)
+        
         sectors = getSectors(distances)
         print(f"Object detected at sectors: {sectors}")
         time.sleep(0.5)
         distances = []
-        clear_lines_from_bottom(4)
+        clear_lines_from_bottom(6)
 except KeyboardInterrupt:
     print("Measurement stopped by user")
 finally:
-    os.system('clear')
+    #os.system('clear')
     GPIO.cleanup()
     for name, ser in lidars_serials.items():
         ser.close()
